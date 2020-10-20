5a10b20ee2969aeb10b0479454a90449d86f554c
shinhwagk <shanghai_sl@hotmail.com>
2020-10-20 14:28:51 +0800

    save

--- test.sh
+++ test.sh
@@ -7,11 +7,10 @@ git init
 mkdir -p /tmp/test/image/1.0.0
 echo  'FROM python:3' > /tmp/test/image/1.0.0/Dockerfile
 mkdir -p /tmp/test/image/1.0.1
-touch a > /tmp/test/image/1.0.1/Dockerfile
+touch 'FROM python:3' > /tmp/test/image/1.0.1/Dockerfile
 touch a > /tmp/test/image/1.0.1/c
 
 git add -A && git commit -m "save"
-export RegistryUsername=shinhwagk
 deno run --allow-env --allow-read --allow-run --allow-net /workspaces/gitbuild/app.ts /tmp/test image
 
 
