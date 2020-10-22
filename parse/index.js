cf98bfa220b0e02830895411ad0d40d5fbe6f79e
gk <shinhwagk@outlook.com>
2020-10-22 14:49:41 +0800

    Create index.js

---
+++ parse/index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const str = '{"context":"images/1.0.0","file":"images/1.0.0/Dockerfile","tags":"user/app:latest"}'
+
+try {
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    core.setOutput("time", str);
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
